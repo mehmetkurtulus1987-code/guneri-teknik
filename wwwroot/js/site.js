6cb2455b11f195b5cf5051351f71d6f0c7a91c31
mehmetkurtulus1987-code <mehmetkurtulus1987@gmail.com>
2026-02-16 14:07:09 +0300

    Update site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -127,11 +127,14 @@ const modal = document.getElementById("kvkkModal");
                 alert("Talebiniz başarıyla alınmıştır. En kısa sürede sizinle iletişime geçeceğiz.");
             }
         });
-        document.addEventListener('click', function(event) {
+        document.addEventListener('pointerdown', function(event) {
     var menuBar = document.getElementById('menu-bar');
-    var nav = document.querySelector('header');
+    var nav = document.querySelector('.nav-container'); // Menü ve ikonları kapsayan iç kutu
 
-    if (menuBar && nav && !nav.contains(event.target) && menuBar.checked) {
-        menuBar.checked = false;
+    // Eğer menü açıksa VE tıklanan yer nav-container değilse VE tıklanan yer ikon değilse
+    if (menuBar && menuBar.checked) {
+        if (nav && !nav.contains(event.target)) {
+            menuBar.checked = false;
+        }
     }
 });
